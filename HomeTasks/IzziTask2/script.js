9796d8d48d01d38cbf22bdc41a28f04e1379ffdb
Leyla Ahmadova <leylaahmadova24@gmail.com>
2024-08-19 23:26:27 +0400

    IzziTask2

--- HomeTasks/IzziTask2/script.js
+++ HomeTasks/IzziTask2/script.js
@@ -2,6 +2,8 @@ let People=document.querySelector(".People")
 let main=document.querySelector("main")
 let Supervisors=document.querySelector("#Supervisors")
 let eliteTasker=document.querySelector("#eliteTasker")
+let New=document.querySelector("#new")
+let Pros=document.querySelector("#Pros")
 let bool1=false;
 let bool2=false;
 let bool3=false;
@@ -13,37 +15,52 @@ let ByTasksCount=document.querySelector("#ByTasksCount")
 async function getInfo(){
     let data= await fetch(`allProsData.json`).then(res=>res.json())
     let people=data.data.taskers;
+    let date=new Date();
+    let currentMonth=date.getMonth()+1;
     let arr1=[]
     let arr2=[]
     let arr3=[]
-    arr1=people.filter((element)=>{
+    let arr4=[]
+        arr1=people.filter((element)=>{
         if(element.supervisor==true){
         return element;
         }
         });
-    arr2=people.filter((element)=>{
-        if(element.eliteTasker==true){
+        arr2=people.filter((element)=>{
+            if(element.eliteTasker==true){
+                return element;
+            }
+        });
+        arr3=people.filter((element)=>{
+            let registerdatemonth=Number(element.startDate.charAt(5)+element.startDate.charAt(6));
+            let difference
+            difference=currentMonth-registerdatemonth
+            if(difference<=3){
+                return element;
+            }
+        });    
+        arr4=people.filter((element)=>{
+            if(element.supervisor==false){
             return element;
             }
         });
-    arr3=people.filter((element)=>{
-        if((element.eliteTasker==true)&&(element.supervisor==true)){
-            return element;
-            }
-        });
+
 function Height(){
-    if((bool1==true)&&(bool2!=true)){
-        main.style.height="1257px"
-    }
-    else if((bool1!=true)&&(bool2==true)){
+    if(eliteTasker.checked==true){
         main.style.height="2157px"
-    }
-    else if(bool1==true&&bool2==true){
+        }
+        else if(New.checked==true){
+        main.style.height="2157px"
+        }
+        else if(Supervisors.checked==true){
         main.style.height="1257px"
-    }
-    else{
+        }
+        else if(Pros.checked==true){
+        main.style.height="3655px"
+        }
+        else{
         main.style.height="4909px"
-    }
+        }
 }
 Height()
 function Arrays(arrays){
@@ -62,158 +79,138 @@ function Arrays(arrays){
                 <div class="user">
                     <img src="${arrays[i].user.profile_picture.publicUrl}" alt="">
                     <div class="nameAndRating">
-                    <p>${arrays[i].user.name} ${surname=arrays[i].user.surname}</p>
-                    <div class="rating">
+                    <div class="namesupervisor">
+                    <p>${arrays[i].user.name} ${arrays[i].user.surname}</p>
+                    <div class="supervisor"></div>
+                    </div>
+                    <div class="ratingandnumber">
+                    <div class="rating"></div>
+                    <div class="ratingnumber">${arrays[i].averageRating}</div>
+                    <div class="number">(24)</div>
                     </div>
                 </div>
                 </div>
                 <div class="info">
-                    <div class="completedTasks"><span>Completed Tasks: </span> ${arrays[i].completedTasks}</div>
-                </div>
-                <div class="bio"><span>Bio: </span>${arrays[i].bio}</div>
-                <div class="details">
-                <div class="supervisor"></div>
+                    <div class="completedTasks"><img src="assets/Group 26286.png" alt=""> ${arrays[i].completedTasks} Tasks</div>
                 <div class="eliteTasker"></div>
+                    <div class="New"></div>
+                </div>
+                <div class="bio">Hi! I am ${arrays[i].user.name}. ${arrays[i].bio}</div>
+                <div class="details">
+                <div class="View">view profile</div>
+                <div class="BuutonAndPrice">
+                <div class="dollar">$35</div>
+                <div class="bookNow">Book Now</div>
+                </div>
                 </div>   
         `
+        let registerdatemonth=Number(arrays[i].startDate.charAt(5)+arrays[i].startDate.charAt(6));
+        let difference
+        difference=currentMonth-registerdatemonth
         People.appendChild(Person)
         let rating=Person.querySelector(".rating")
         rating.appendChild(Rating)
-        if(arrays[i].supervisor==true){
-            let supervisor=Person.querySelector(".supervisor")
-            supervisor.textContent='Supervisor'
-        }
         if(arrays[i].eliteTasker==true){
             let eliteTasker=Person.querySelector(".eliteTasker")
-            eliteTasker.textContent='eliteTasker'
+            eliteTasker.innerHTML='<img src="assets/Hashtag.svg" alt=""> Top Pro'
+        }
+        if(arrays[i].supervisor==true){
+            let supervisor=Person.querySelector(".supervisor")
+            supervisor.textContent=`(supervisor)`
+        }
+        if(difference<=3){
+            let New=Person.querySelector(".New")
+            New.innerHTML=`<img src="assets/Shape.png" alt=""> New Pro`
         }   
     }
 }
-function boolonetwo(aaa,bbb,ccc,ddd){
-if((bool1==true)&&(bool2!=true)){         
+function boolonetwo(aaa,bbb,ccc,eee,ddd){
+if(eliteTasker.checked==true){         
 Arrays(aaa)  
 }
-else if((bool1!=true)&&(bool2==true)){
+else if(New.checked==true){
 Arrays(bbb)
 }
-else if(bool1==true&&bool2==true){
+else if(Supervisors.checked==true){
 Arrays(ccc)
+}
+else if(Pros.checked==true){
+Arrays(eee)
 }
 else{
 Arrays(ddd)
 }
 }
 Arrays(people)
-Supervisors.addEventListener("click",()=>{   
-        if(((bool3==true)&&(bool4!=true))||((bool3==true)&&(bool4==true))){  
-            if(Supervisors.checked==true){
-                bool1=true
-            }
-            else{
-                bool1=false
-            }
-            boolonetwo(arr1.sort((a,b)=>a.averageRating-b.averageRating),arr2.sort((a,b)=>a.averageRating-b.averageRating),arr3.sort((a,b)=>a.averageRating-b.averageRating),people.sort((a,b)=>a.averageRating-b.averageRating))
-        }
-        else if((bool3!=true)&&(bool4==true)){
-            if(Supervisors.checked==true){
-                bool1=true
-            }
-            else{
-                bool1=false
-            }
-            boolonetwo(arr1.sort((a,b)=>a.completedTasks-b.completedTasks),arr2.sort((a,b)=>a.completedTasks-b.completedTasks),arr3.sort((a,b)=>a.completedTasks-b.completedTasks),people.sort((a,b)=>a.completedTasks-b.completedTasks))
-        }
-        else if(descending_rating=="descending"){
-            if(Supervisors.checked==true){
-                bool1=true
-            }
-            else{
-                bool1=false
-            }
-            boolonetwo(arr1.sort((a,b)=>b.averageRating-a.averageRating),arr2.sort((a,b)=>b.averageRating-a.averageRating),arr3.sort((a,b)=>b.averageRating-a.averageRating),people.sort((a,b)=>b.averageRating-a.averageRating))
-        }
-        else if(descending_count=="descending"){
-            if(Supervisors.checked==true){
-            bool1=true
-        }
-        else{
-            bool1=false
-        }
-            boolonetwo(arr1.sort((a,b)=>b.completedTasks-a.completedTasks),arr2.sort((a,b)=>b.completedTasks-a.completedTasks),arr3.sort((a,b)=>b.completedTasks-a.completedTasks),people.sort((a,b)=>b.completedTasks-a.completedTasks))
-        }     
-        else{
-            if(Supervisors.checked==true){
-                bool1=true
-            }
-            else{
-                bool1=false
-            }
-            boolonetwo(arr1,arr2,arr3,people)
-        }
+
+New.addEventListener("click",()=>{
+    if(New.checked==true){
+        eliteTasker.checked=false
+        Pros.checked=false
+        Supervisors.checked=false
+        Arrays(arr3)
         Height()
-})
-eliteTasker.addEventListener("click",()=>{
-    if(((bool3==true)&&(bool4!=true))||((bool3==true)&&(bool4==true))){
-        if(eliteTasker.checked==true){
-            bool2=true
-        }
-        else{
-            bool2=false
-        }
-        boolonetwo(arr1.sort((a,b)=>a.averageRating-b.averageRating),arr2.sort((a,b)=>a.averageRating-b.averageRating),arr3.sort((a,b)=>a.averageRating-b.averageRating),people.sort((a,b)=>a.averageRating-b.averageRating))
-    }
-    else if((bool3!=true)&&(bool4==true)){
-        if(eliteTasker.checked==true){
-            bool2=true
-        }
-        else{
-            bool2=false
-        }  
-        boolonetwo(arr1.sort((a,b)=>a.completedTasks-b.completedTasks),arr2.sort((a,b)=>a.completedTasks-b.completedTasks),arr3.sort((a,b)=>a.completedTasks-b.completedTasks),people.sort((a,b)=>a.completedTasks-b.completedTasks))
-    }
-    else if(descending_rating=="descending"){
-        if(Supervisors.checked==true){
-            bool1=true
-        }
-        else{
-            bool1=false
-        }
-        boolonetwo(arr1.sort((a,b)=>b.averageRating-a.averageRating),arr2.sort((a,b)=>b.averageRating-a.averageRating),arr3.sort((a,b)=>b.averageRating-a.averageRating),people.sort((a,b)=>b.averageRating-a.averageRating))
-    }
-    else if(descending_count=="descending"){
-        if(eliteTasker.checked==true){
-            bool2=true
-        }
-        else{
-            bool2=false
-        }
-        boolonetwo(arr1.sort((a,b)=>b.completedTasks-a.completedTasks),arr2.sort((a,b)=>b.completedTasks-a.completedTasks),arr3.sort((a,b)=>b.completedTasks-a.completedTasks),people.sort((a,b)=>b.completedTasks-a.completedTasks))
     }
     else{
-        if(eliteTasker.checked==true){
-            bool2=true
-        }
-        else{
-            bool2=false
-        }
-        boolonetwo(arr1,arr2,arr3,people)                       
-    }
-    Height()
+        Arrays(people)
+        Height()
+    }  
 })
+Pros.addEventListener("click",()=>{
+    if(Pros.checked==true){
+        eliteTasker.checked=false
+        Supervisors.checked=false
+        New.checked=false
+        Arrays(arr4)
+        Height()
+    } 
+    else{
+        Arrays(people)
+        Height()
+    }     
+})
+eliteTasker.addEventListener("click",()=>{
+    if(eliteTasker.checked==true){
+        Supervisors.checked=false
+        Pros.checked=false
+        New.checked=false
+        Arrays(arr2)
+        Height()
+    } 
+    else{
+        Arrays(people)
+        Height()
+    }     
+})
+Supervisors.addEventListener("click",()=>{
+    if(Supervisors.checked==true){
+        eliteTasker.checked=false
+        Pros.checked=false
+        New.checked=false
+        Arrays(arr1)
+        Height()
+    }
+    else{
+        Arrays(people)
+        Height()
+    }  
+})
+
 ByRating.addEventListener("click",()=>{
     if(ByRating.value=="Ascending"){ 
         bool3=true
         bool4=false
         descending_rating="ascending"
         descending_count=""
-        boolonetwo(arr1.sort((a,b)=>a.averageRating-b.averageRating),arr2.sort((a,b)=>a.averageRating-b.averageRating),arr3.sort((a,b)=>a.averageRating-b.averageRating),people.sort((a,b)=>a.averageRating-b.averageRating))
+        boolonetwo(arr2.sort((a,b)=>a.averageRating-b.averageRating),arr3.sort((a,b)=>a.averageRating-b.averageRating),arr1.sort((a,b)=>a.averageRating-b.averageRating),arr4.sort((a,b)=>a.averageRating-b.averageRating),people.sort((a,b)=>a.averageRating-b.averageRating))
     }
     else{
         bool3=false
         bool4=false
         descending_rating="descending"
         descending_count=""
-        boolonetwo(arr1.sort((a,b)=>b.averageRating-a.averageRating),arr2.sort((a,b)=>b.averageRating-a.averageRating),arr3.sort((a,b)=>b.averageRating-a.averageRating),people.sort((a,b)=>b.averageRating-a.averageRating))
+        boolonetwo(arr2.sort((a,b)=>b.averageRating-a.averageRating),arr3.sort((a,b)=>b.averageRating-a.averageRating),arr1.sort((a,b)=>b.averageRating-a.averageRating),arr4.sort((a,b)=>b.averageRating-a.averageRating),people.sort((a,b)=>b.averageRating-a.averageRating))
+
     }
 })
 ByTasksCount.addEventListener("click",()=>{
@@ -222,14 +219,15 @@ ByTasksCount.addEventListener("click",()=>{
         bool3=false
         descending_count="ascending"
         descending_rating=""
-        boolonetwo(arr1.sort((a,b)=>a.completedTasks-b.completedTasks),arr2.sort((a,b)=>a.completedTasks-b.completedTasks),arr3.sort((a,b)=>a.completedTasks-b.completedTasks),people.sort((a,b)=>a.completedTasks-b.completedTasks))
+        boolonetwo(arr2.sort((a,b)=>a.completedTasks-b.completedTasks),arr3.sort((a,b)=>a.completedTasks-b.completedTasks),arr1.sort((a,b)=>a.completedTasks-b.completedTasks),arr4.sort((a,b)=>a.completedTasks-b.completedTasks),people.sort((a,b)=>a.completedTasks-b.completedTasks))
     }
     else{
         bool4=false
         bool3=false
         descending_count="descending"
         descending_rating=""
-        boolonetwo(arr1.sort((a,b)=>b.completedTasks-a.completedTasks),arr2.sort((a,b)=>b.completedTasks-a.completedTasks),arr3.sort((a,b)=>b.completedTasks-a.completedTasks),people.sort((a,b)=>b.completedTasks-a.completedTasks))
+        boolonetwo(arr2.sort((a,b)=>b.completedTasks-a.completedTasks),arr3.sort((a,b)=>b.completedTasks-a.completedTasks),arr1.sort((a,b)=>b.completedTasks-a.completedTasks),arr4.sort((a,b)=>b.completedTasks-a.completedTasks),people.sort((a,b)=>b.completedTasks-a.completedTasks))
+
     }
 })
 }
